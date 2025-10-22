c44329aae20e00ec2bae7170c810657f0bfb1004
Joe Haynes <JoeHaynes@sandiego.edu>
2025-10-22 14:00:24 -0700

    feature: scaffold for chat view functions

---
+++ src/js/view.js
@@ -0,0 +1,42 @@
+export function View(doc){
+    let d = document;
+    let list = d.querySelector(".messages")
+    let form = d.querySelector(".input-area");
+    let input = form.querySelector(".input");
+    let count = d.getElementById("msgCount");
+    let exptButton = d.getElementById("exportButton");
+    let impButton = d.getElementById("importButton");
+    let clrButton = d.getElementById("clearButton");
+
+    let onSend = null;
+    let onDelete = null;
+    let onEdit = null;
+    let onImport = null;
+    let onExport = null;
+    let onClear = null;
+
+    function render(state){
+
+    }
+
+    if (form){
+
+    }
+
+    if (exptButton){
+
+    }
+
+    if (impButton){
+
+    }
+
+    if (clrButton){
+
+    }
+
+    return{
+
+    }
+
+}
